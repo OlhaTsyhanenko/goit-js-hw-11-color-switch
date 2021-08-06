f9468934f144f14b4327e072de673bc0796fabd8
Olha Tsyhanenko <olya.tsyganenko@gmail.com>
2021-08-06 15:12:20 +0300

    финальный код

--- js/color-switcher.js
+++ js/color-switcher.js
@@ -6,3 +6,34 @@ const colors = [
     '#009688',
     '#795548',
 ];
+
+const randomIntegerFromInterval = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
+const btnOpen = document.querySelector('button[data-action="start"]');
+const btnStop = document.querySelector('button[data-action="stop"]');
+const bodyEl = document.querySelector('body');
+const NOTIFICATION_DELAY = 1000;
+let intervalId = undefined;
+    
+
+btnOpen.addEventListener('click', onButtonOpen);
+btnStop.addEventListener('click', onButtonStop);
+
+function onButtonOpen(evt) {
+    showChangebackgroundColor();
+}
+
+function showChangebackgroundColor() {
+    intervalId = setInterval(() => {
+        console.log("меняем фон",randomIntegerFromInterval(0, colors.length));
+        bodyEl.style.backgroundColor = colors[randomIntegerFromInterval(0, colors.length)]; 
+    }, NOTIFICATION_DELAY);
+    btnOpen.setAttribute('disabled', true);
+}
+ 
+function onButtonStop() {
+    clearInterval(intervalId);
+    btnOpen.removeAttribute('disabled');
+}
